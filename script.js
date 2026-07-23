e4c2f8eb63461ca8b38b7f53e032de4a8d9a95f4
ttSamtopia <s-linden@live.se>
2026-07-23 11:18:59 +0200

    add function for computerChoice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber <= 0.33) { return "Rock"; }
+    else if (randomNumber > 0.33 && randomNumber <= 0.66) { return "Paper"; }
+    else { return "Scissors"; }
+}
